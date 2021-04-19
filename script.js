bb777ceffb55c345cb4850c5e1f8ff297cdc1982
nicolamilani1988 <nicola.milani1988@outlook.it>
2021-04-19 16:29:40 +0200

    creata ricerca per serie TV

--- script.js
+++ script.js
@@ -3,38 +3,68 @@ function init(){
         el: '#app',
         data:{
             films: '',
+            series: '',
             searchedFilm: '',
             flags:['de','en','it'],
         },
         methods:{
             getFilms: function(){
-                axios.get('https://api.themoviedb.org/3/search/movie',
-                {
-                    params: {
-                        api_key: '08129c0589bf0f473da03e334eb1d88a',
-                        language: 'it',
-                        query: this.searchedFilm // da sostituire col valore v-model dell'input
-                    }
-                })
-                .then(data => {
+                const api_key = '08129c0589bf0f473da03e334eb1d88a';
+                const query = this.searchedFilm;
+                const language = 'it';
 
-                    this.films = data['data']['results'];
+                axios.all([
+                    axios.get('https://api.themoviedb.org/3/search/movie',
+                        {
+                            params: {
+                                api_key ,
+                                language ,
+                                query
+                            }
+                        }),
+                    
+                    axios.get('https://api.themoviedb.org/3/search/tv',
+                    {
+                        params: {
+                            api_key ,
+                            language ,
+                            query
+                        }
+                    })    
+                ])
+                
+                .then(axios.spread((search1 , search2) => {
+
+                    this.films = search1['data']['results'];
+                    this.series = search2['data']['results'];
                     console.log(this.films);
-                    console.log(this.films[0]['title']);
-
-                })
+                    console.log(this.series);
+                                
+                }))
                 .catch(() => console.log('error'))
             },
        
             getTitle: function(val){
                 if (val != ''){
-                        return val['title'];                    
-                }           
+                    return val['title'];
+                }                  
+            },
+
+            getName: function(val){
+                if (val != ''){
+                    return val['name'];
+                } 
             },
 
             getOriginalTitle: function(val){
                 if (val != ''){
                     return val['original_title'];
+                }                        
+            },
+
+            getOriginalName: function(val){
+                if(val != ''){
+                    return val['original_name'];
                 }
             },
 
@@ -50,8 +80,8 @@ function init(){
                 }
             }, 
 
-            getFlag: function(film){        
-                const language = this.getOriginalLanguage(film);
+            getFlag: function(value){        
+                const language = this.getOriginalLanguage(value);
                 if(this.flags.includes(language)){
                     return language;
                 } else{
